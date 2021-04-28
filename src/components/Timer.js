c1f9d580f94a27d147d5164938bba4e9664865a8
AlexeyLocker <alexey.locker@gmail.com>
2021-04-28 23:34:03 +0300

    Function component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Subject, interval } from "rxjs";
 import { map, debounceTime, buffer, filter } from "rxjs/operators";
 import "./Timer.css";
@@ -8,84 +8,68 @@ const interval$ = interval(1000);
 const stop$ = new Subject();
 const start$ = new Subject();
 const wait$ = new Subject();
+const reset$ = new Subject();
 
-export default class Timer extends Component {
-    state = {
-        second: 0,
-        minute: 0,
-        hour: 0,
-    };
+const DEFAULT_TIMER = {
+    second: 0,
+    minute: 0,
+    hour: 0,
+}
+export default function Timer() {
+    const [timer, setTimer] = useState(DEFAULT_TIMER)
 
-    started = false;
-    subs = null;
 
-    render() {
-        return (
-            <div className="timer">
-                <p>
-                    { this.state.hour < 10 ? `0` : null }
-                    { this.state.hour }:{ this.state.minute < 10 ? `0` : null }
-                    { this.state.minute }:{ this.state.second < 10 ? `0` : null }
-                    { this.state.second }
-                </p>
-                <div className="timer__controls">
-                    <input type="button" value="Start" onClick={ () => { start$.next(); } } />
-                    <input type="button" value="Stop" onClick={ () => { stop$.next(); } } />
-                    <input type="button" value="Wait" onClick={ () => { wait$.next(); } } />
-                    <input type="button" value="Reset" onClick={ () => { stop$.next("Reset"); } } />
-                </div>
-            </div>
-        );
-    }
-    componentDidMount() {
-        start$.subscribe((val) => {
-            if (!this.started) {
-                this.started = true;
-            } else {
-                return null;
+    const addSecond = useCallback(() => {
+        setTimer((timer) => {
+            if (timer.second + 1 === 60) {
+                if (timer.minute + 1 === 60) {
+                    return { second: 0, minute: 0, hour: timer.hour + 1 }
+                } else {
+                    return { ...timer, second: 0, minute: timer.minute + 1 }
+                }
             }
-            this.subs = interval$
-                .pipe(
-                    map((val) => {
-                        return this.state.second === 60 ? 0 : this.state.second + 1;
-                    })
-                )
-                .subscribe((val) => {
-                    if (val === 0) {
-                        if (this.state.minute === 59) {
-                            this.setState({ hour: this.state.hour + 1 });
-                            this.setState({ minute: 0 });
-                        } else {
-                            this.setState({ minute: this.state.minute + 1 });
-                        }
-                    }
-                    this.setState({ second: val });
-                });
-        });
-        stop$.subscribe((val) => {
-            if (val === "Reset" && this.subs) {
-                this.subs.unsubscribe();
-                this.started = false;
-                start$.next();
-            } else if (this.subs) {
-                this.subs.unsubscribe();
-                this.started = false;
-            }
-            this.setState({
-                second: 0,
-                minute: 0,
-                hour: 0,
-            });
-        });
+            return { ...timer, second: timer.second + 1 }
+        })
+    }, [])
 
+    useEffect(() => {
+        let subscription;
+        start$.subscribe(() => {
+            subscription = interval$.subscribe(addSecond);
+        })
+        stop$.subscribe(() => {
+            subscription.unsubscribe()
+            setTimer(DEFAULT_TIMER);
+        })
+        reset$.subscribe(() => {
+            setTimer(DEFAULT_TIMER);
+        })
         wait$
             .asObservable()
-            .pipe(buffer(wait$.pipe(debounceTime(300))))
+            .pipe(buffer(wait$.pipe(debounceTime(250))))
             .pipe(map((click) => click.length))
             .pipe(filter((click) => click === 2))
             .subscribe(() => {
-                this.started = false;
-                if (this.subs) this.subs.unsubscribe();
+                subscription.unsubscribe(addSecond)
             });
-    }
+    }, [addSecond])
+
+
+    return (
+        <div className="timer">
+            <p>
+                { timer.hour < 10 ? `0` : null }
+                { timer.hour }:{ timer.minute < 10 ? `0` : null }
+                { timer.minute }:{ timer.second < 10 ? `0` : null }
+                { timer.second }
+            </p>
+            <div className="timer__controls">
+                <input type="button" value="Start" onClick={ () => { start$.next(); } } />
+                <input type="button" value="Stop" onClick={ () => { stop$.next(); } } />
+                <input type="button" value="Wait" onClick={ () => { wait$.next(); } } />
+                <input type="button" value="Reset" onClick={ () => { reset$.next(); } } />
+            </div>
+        </div>
+    );
 }
+
